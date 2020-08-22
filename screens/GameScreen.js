afd8e859de63df4fd8bde1539d7dac0d258badf4
didulidu <44614677+dovlaper@users.noreply.github.com>
2020-08-22 19:00:48 +0200

    implemented gravity, and bird component

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,4 +1,4 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import { GameEngine } from 'react-native-game-engine';
 import Matter from 'matter-js';
